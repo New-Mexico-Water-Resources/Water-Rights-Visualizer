f6d51c56e9847328f33fb72a29735cb3d6b3759a
Gregory Halverson <gregory.h.halverson@jpl.nasa.gov>
2024-01-29 14:24:17 -0800

    main for tk gui

--- water_rights_visualizer/water_rights_gui_tk.py
+++ water_rights_visualizer/water_rights_gui_tk.py
@@ -169,3 +169,40 @@ def water_rights_gui_tk(
     submit_button.place(relx=0.670, rely=0.4, relheight=0.05)
 
     root.mainloop()
+
+def main(argv=sys.argv):
+    if "--boundary-filename" in argv:
+        boundary_filename = str(argv[argv.index("--boundary-filename") + 1])
+    else:
+        boundary_filename = None
+
+    if "--output-directory" in argv:
+        output_directory = str(argv[argv.index("--output-directory") + 1])
+    else:
+        output_directory = None
+
+    if "--input-directory" in argv:
+        input_directory = str(argv[argv.index("--input-directory") + 1])
+    else:
+        input_directory = None
+
+    if "--start-year" in argv:
+        start_year = str(argv[argv.index("--start-year") + 1])
+    else:
+        start_year = None
+
+    if "--end-year" in argv:
+        end_year = str(argv[argv.index("--end-year") + 1])
+    else:
+        end_year = None
+
+    water_rights_gui_tk(
+        boundary_filename=boundary_filename,
+        output_directory=output_directory,
+        input_directory=input_directory,
+        start_year=start_year,
+        end_year=end_year
+    )
+
+if __name__ == "__main__":
+    main(argv=sys.argv)
