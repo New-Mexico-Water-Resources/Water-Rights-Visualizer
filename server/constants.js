6b8fa1c18983ff8cd16267c25c537f850ed62265
Gregory Halverson <gregory.h.halverson@jpl.nasa.gov>
2024-05-06 15:20:03 -0700

    fixing docker directories

--- server/constants.js
+++ server/constants.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const port = 8000;
 const server_directory = __dirname;
-const project_directory = path.dirname(__dirname);
+
+// Use the first command line argument as the project directory if it exists, otherwise use __dirname
+const project_directory = process.argv[2] ? path.resolve(process.argv[2]) : __dirname;
+
 const run_directory_base = path.join(project_directory, 'runs');
 
 module.exports = {
