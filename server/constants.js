e4eeb9833c86020fa19887761187326d8dbe157c
Gregory Halverson <gregory.h.halverson@jpl.nasa.gov>
2024-05-07 19:35:19 -0700

    fixing project directory and child processes

--- server/constants.js
+++ server/constants.js
@@ -1,11 +1,8 @@
 const path = require('path');
 const port = 8000;
 const server_directory = __dirname;
-
-// Use the first command line argument as the project directory if it exists, otherwise use __dirname
-const project_directory = process.argv[2] ? path.resolve(process.argv[2]) : __dirname;
-
-const run_directory_base = path.join(project_directory, 'runs');
+const project_directory = path.dirname(__dirname);
+const run_directory_base = path.join(project_directory, '/data/water_rights_runs');
 
 module.exports = {
   port,
