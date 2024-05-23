6f93621c0b8f19a4b36da25d083cef7ca5e4e006
fhy <fhy@jpl.nasa.gov>
2024-05-23 16:46:09 -0700

    updated report queue to invoke commands

---
+++ water_report_queue.py
@@ -0,0 +1,111 @@
+import subprocess
+import time
+import os
+import argparse
+import json
+import subprocess
+
+from datetime import datetime
+
+DEFAULT_QUEUE = "/root/data/water_rights_runs/report_queue.json"
+
+#some things we need for cleanup...
+#reset In Progress to Pending in case things get hung
+#clear out "Failed" runs
+#clear out json file completely
+#read from an "archived_runs.json"
+
+class WaterReportException(Exception):
+    pass
+
+def exec_report(record):                       
+    cmd = record['cmd'].split(" ")
+    print("invoking cmd: {}".format(cmd))
+
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+
+    if err:
+        output = err.decode()
+        return output
+
+    return "Invoked without errors: {}".format(cmd)
+
+def update_status(record, state):
+    now = datetime.now()
+    
+    record['status'] = state
+    
+    if state == "In Progress":
+        record['started'] = str(now)    
+    elif state == "Invoked without errors":
+        record['ended'] = str(now)
+    #might have to do special stuff like retry later so put in seperate elif
+    elif state == "Failed":        
+        record['ended'] = str(now)
+        
+#completely overwrite the contents of the queue_file_path with queue_data
+def update_queue_file(queue_file_path, queue_data):
+    with open(queue_file_path, 'w') as queue_file:
+        queue_file.seek(0)
+        queue_file.write(json.dumps(queue_data, indent=4))
+        queue_file.truncate()
+                
+def check_report_queue(queue_file_path):    
+    print("Reading queue_file from {}".format(queue_file_path))
+    
+    try:
+        queue_data = []
+        with open(queue_file_path, 'r') as queue_file:
+            queue_data = json.load(queue_file)
+        
+        for record in queue_data:
+            if record['status'] == "Pending":                    
+                update_status(record, "In Progress")
+
+                #update the queue file right away so we see the "In Progress"
+                update_queue_file(queue_file_path, queue_data)
+
+                status_msg = exec_report(record)
+                record['status_msg'] = status_msg
+                
+                status = None
+                if status_msg.startswith("Invoked without errors"):
+                    status = "Invoked without errors"
+                else:
+                    status = "Failed"
+                    
+                update_status(record, status)
+                
+                #update the queue file again with the final status
+                update_queue_file(queue_file_path, queue_data)    
+    except FileNotFoundError as e:
+        print("Report Queue File not found: {}".format(queue_file_path))
+    
+def check_already_running():
+    cmd = ['pgrep -f .*python.*water_report_queue.py']
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, 
+    stderr=subprocess.PIPE)
+    my_pid, err = process.communicate()
+
+    if len(my_pid.splitlines()) >0:
+        print("Water report queue is already running")
+        exit()
+
+def main():
+    global DEFAULT_QUEUE
+    
+    parser = argparse.ArgumentParser(description='Read off the report queue and invoke water-rights-visualizer-backend')
+    parser.add_argument('-q', '--queue', default=DEFAULT_QUEUE, help='path to queue location')
+    
+    args = parser.parse_args()
+    queue = args.queue
+    
+    check_already_running()
+    
+    while True:
+        check_report_queue(queue)
+        time.sleep(60)        
+    
+if __name__ == "__main__":
+    main()
