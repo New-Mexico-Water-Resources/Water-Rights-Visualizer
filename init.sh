dac91dd0539c5c20a2999c262ea812cf6d8356ae
fhy <fhy@jpl.nasa.gov>
2024-05-29 15:41:33 -0700

    added npm command into init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 /etc/init.d/cron start
-/opt/conda/bin/python /app/water_report_queue.py >> /tmp/cron_log.txt 2>&1
-tail -f /tmp/cron_log.txt
+echo "Starting python queue watcher"
+/opt/conda/bin/python /app/water_report_queue.py >> /tmp/cron_log.txt 2>&1 &
+echo "Starting npm with $1"
+npm run $
